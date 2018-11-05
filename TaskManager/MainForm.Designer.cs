0fe25e2cbd3cc0dfef92a95aca88ac7372e622c0
Makc <mks59k@gmail.com>
2018-11-05 23:26:56 +0200

    Визуальное оформление меню.

--- TaskManager/MainForm.Designer.cs
+++ TaskManager/MainForm.Designer.cs
@@ -30,22 +30,29 @@
         {
             this.listViewActiveProcesses = new System.Windows.Forms.ListView();
             this.buttonUpdate = new System.Windows.Forms.Button();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusCountProcess = new System.Windows.Forms.ToolStripStatusLabel();
-            this.statusStrip1.SuspendLayout();
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.controlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startProcToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.endProcToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip.SuspendLayout();
+            this.menuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
             // listViewActiveProcesses
             // 
-            this.listViewActiveProcesses.Location = new System.Drawing.Point(12, 51);
+            this.listViewActiveProcesses.Location = new System.Drawing.Point(12, 61);
             this.listViewActiveProcesses.Name = "listViewActiveProcesses";
-            this.listViewActiveProcesses.Size = new System.Drawing.Size(539, 221);
+            this.listViewActiveProcesses.Size = new System.Drawing.Size(539, 214);
             this.listViewActiveProcesses.TabIndex = 0;
             this.listViewActiveProcesses.UseCompatibleStateImageBehavior = false;
             // 
             // buttonUpdate
             // 
-            this.buttonUpdate.Location = new System.Drawing.Point(476, 22);
+            this.buttonUpdate.Location = new System.Drawing.Point(476, 32);
             this.buttonUpdate.Name = "buttonUpdate";
             this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
             this.buttonUpdate.TabIndex = 1;
@@ -53,15 +60,15 @@
             this.buttonUpdate.UseVisualStyleBackColor = true;
             this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
             // 
-            // statusStrip1
+            // statusStrip
             // 
-            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusCountProcess});
-            this.statusStrip1.Location = new System.Drawing.Point(0, 278);
-            this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(563, 22);
-            this.statusStrip1.TabIndex = 2;
-            this.statusStrip1.Text = "statusStrip1";
+            this.statusStrip.Location = new System.Drawing.Point(0, 283);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(563, 22);
+            this.statusStrip.TabIndex = 2;
+            this.statusStrip.Text = "statusStrip";
             // 
             // toolStripStatusCountProcess
             // 
@@ -69,18 +76,68 @@
             this.toolStripStatusCountProcess.Size = new System.Drawing.Size(103, 17);
             this.toolStripStatusCountProcess.Text = "toolStripStatusLabel";
             // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.controlToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(563, 24);
+            this.menuStrip.TabIndex = 3;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.menuToolStripMenuItem.Text = "Меню";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Выход";
+            // 
+            // controlToolStripMenuItem
+            // 
+            this.controlToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.startProcToolStripMenuItem,
+            this.endProcToolStripMenuItem});
+            this.controlToolStripMenuItem.Name = "controlToolStripMenuItem";
+            this.controlToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.controlToolStripMenuItem.Text = "Управление";
+            // 
+            // startProcToolStripMenuItem
+            // 
+            this.startProcToolStripMenuItem.Name = "startProcToolStripMenuItem";
+            this.startProcToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.startProcToolStripMenuItem.Text = "Запуск программы по расписанию";
+            // 
+            // endProcToolStripMenuItem
+            // 
+            this.endProcToolStripMenuItem.Name = "endProcToolStripMenuItem";
+            this.endProcToolStripMenuItem.Size = new System.Drawing.Size(273, 22);
+            this.endProcToolStripMenuItem.Text = "Завершение программы по расписанию";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(563, 300);
-            this.Controls.Add(this.statusStrip1);
+            this.ClientSize = new System.Drawing.Size(563, 305);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip);
             this.Controls.Add(this.buttonUpdate);
             this.Controls.Add(this.listViewActiveProcesses);
+            this.MainMenuStrip = this.menuStrip;
             this.Name = "MainForm";
             this.Text = "Task Manager";
-            this.statusStrip1.ResumeLayout(false);
-            this.statusStrip1.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,8 +147,14 @@
 
         private System.Windows.Forms.ListView listViewActiveProcesses;
         private System.Windows.Forms.Button buttonUpdate;
-        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.StatusStrip statusStrip;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusCountProcess;
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem controlToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem startProcToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem endProcToolStripMenuItem;
     }
 }
 
