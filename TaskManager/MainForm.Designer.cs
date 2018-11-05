12e2bb9c32319889ed8e1b53214c6f999ec90775
Makc <mks59k@gmail.com>
2018-11-05 23:19:28 +0200

    Реализовано обновление активных процессов при нажатии на кнопку.

--- TaskManager/MainForm.Designer.cs
+++ TaskManager/MainForm.Designer.cs
@@ -29,31 +29,69 @@
         private void InitializeComponent()
         {
             this.listViewActiveProcesses = new System.Windows.Forms.ListView();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusCountProcess = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // listViewActiveProcesses
             // 
             this.listViewActiveProcesses.Location = new System.Drawing.Point(12, 51);
             this.listViewActiveProcesses.Name = "listViewActiveProcesses";
-            this.listViewActiveProcesses.Size = new System.Drawing.Size(419, 237);
+            this.listViewActiveProcesses.Size = new System.Drawing.Size(539, 221);
             this.listViewActiveProcesses.TabIndex = 0;
             this.listViewActiveProcesses.UseCompatibleStateImageBehavior = false;
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(476, 22);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 1;
+            this.buttonUpdate.Text = "Обновить";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusCountProcess});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 278);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(563, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusCountProcess
+            // 
+            this.toolStripStatusCountProcess.Name = "toolStripStatusCountProcess";
+            this.toolStripStatusCountProcess.Size = new System.Drawing.Size(103, 17);
+            this.toolStripStatusCountProcess.Text = "toolStripStatusLabel";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 300);
+            this.ClientSize = new System.Drawing.Size(563, 300);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonUpdate);
             this.Controls.Add(this.listViewActiveProcesses);
             this.Name = "MainForm";
             this.Text = "Task Manager";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.ListView listViewActiveProcesses;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusCountProcess;
     }
 }
 
--- TaskManager/MainForm.cs
+++ TaskManager/MainForm.cs
@@ -2,19 +2,74 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Timer = System.Threading.Timer;
 
 namespace TaskManager
 {
     public partial class MainForm : Form
     {
+        private List<ListViewItem> listViewItems = null;
+
         public MainForm()
         {
             InitializeComponent();
+
+            this.Load += MainForm_Load;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.listViewActiveProcesses.View = View.Details;
+
+            this.listViewActiveProcesses.Columns.Add("Имя процесса", 140);
+            this.listViewActiveProcesses.Columns.Add("ID", 40);
+            this.listViewActiveProcesses.Columns.Add("Память", 100);
+            
+            listViewItems = new List<ListViewItem>();
+
+            this.ShowActiveProcesses();
+
+            //TimerCallback timerCallback = new TimerCallback(this.ShowActiveProcesses);
+            //Timer timer = new Timer(timerCallback);
+            //timer.Change(0, 3000);
+        }
+
+        private void ShowActiveProcesses()
+        {
+            this.listViewActiveProcesses.Items.Clear();
+            this.listViewItems.Clear();
+
+            Process[] processes = Process.GetProcesses();
+
+            foreach (Process process in Process.GetProcesses())
+            {
+                listViewItems.Add(
+                    new ListViewItem(
+                        new string[] {
+                                process.ProcessName,
+                                process.Id.ToString(),
+                                process.VirtualMemorySize64.ToString("0,0,0", CultureInfo.InvariantCulture)
+                            }
+                    )
+                );
+            }
+
+            this.listViewActiveProcesses.Items.AddRange(listViewItems.ToArray());
+
+            this.toolStripStatusCountProcess.Text = "Процессов: " + listViewItems.Count;
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            this.ShowActiveProcesses();
         }
     }
 }
